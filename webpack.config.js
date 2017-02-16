1673aeb26ae90e0434f2b11fe37204ed4b4194a1
duanguang <492466509@qq.com>
2017-02-17 00:04:21 +0800

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by duanguang on 2017/2/17.
+ */
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./webpack.prod.config')
+} else {
+    module.exports = require('./webpack.dev.config')
+}
